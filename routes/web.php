1ff8397981fb6e7ed96f300bd282b3d6fc40334c
Jaime García <jaime.garcia@iesdonana.org>
2024-11-28 12:11:36 +0100

    Rutas añadidas a web php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticuloController;
+use App\Http\Controllers\FacturaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('articulos', ArticuloController::class);
+Route::resource('facturas', FacturaController::class);
 
 require __DIR__.'/auth.php';
